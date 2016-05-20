c99ed9d498a1dde9aff4ab248482abb945813120
Viet Hoang <hello@vietamins.com>
2016-05-20 16:07:50 -0700

    Working randomizing of next sentence after complete entry; quick and dirty

--- _assets/javascripts/components/ime-practice/master.js
+++ _assets/javascripts/components/ime-practice/master.js
@@ -9,7 +9,7 @@ var AppBox = React.createClass({
         "eng": ''
       },
       activeSentenceCharacterGroups: [],
-      activeCharacterGroupSet: {},
+      activeCharacterGroup: {},
       currentSentencePosition: 0,
       isInputIncorrect: false
     };
@@ -20,7 +20,21 @@ var AppBox = React.createClass({
       dataType: 'json',
       cache: false,
       success: function(data) {
-        this.setState({sentences: data});
+        var placeholder = [
+          {
+            "id": 75,
+            "bun": "今日は、胃腸の調子が悪いです。",
+            "kana": "きょうは、いちょうのちょうしがわるいです。",
+            "eng": "My stomach is upset today."
+          },
+          {
+            "id": 13,
+            "bun": "私は、妻のことをもう好きじゃないのかもしれない。",
+            "kana": "わたしは、つまのことをもうすきじゃないのかもしれない。",
+            "eng": "I may not love my wife anymore."
+          }
+        ]
+        this.setState({sentences: placeholder});
         this.setActiveSentence();
       }.bind(this),
       error: function(xhr, status, err) {
@@ -29,219 +43,250 @@ var AppBox = React.createClass({
     });
   },
   handleInputCheck: function(input) {
-    var correctInput = this.state.activeCharacterGroupSet.romaji;
-    var correctInputSubstring = correctInput.substring(0, input.length)
-    var isInputIncorrect;
+    var correctInputs = this.state.activeCharacterGroup.romajis;
+    var correctInputsSet = new Set(correctInputs);
 
-    if (input != correctInputSubstring) {
-      isInputIncorrect = true;
-    } else {
-      isInputIncorrect = false;
+    // Check if the user input completely matches with the current character group acceptable romaji
+    if (correctInputsSet.has(input)) {
+      return this.handleInputComplete();
     }
 
+    // If the user input doesn't completely match, check if it is a substring match
+    // If not, send an incorrect input flag
+    var correctInputsSubstrings = correctInputs.map(function(romaji) {
+      return romaji.substring(0, input.length);
+    });
+    var correctInputsSubstringsSet = new Set(correctInputsSubstrings);
+    var isInputIncorrect;
+
+    if (correctInputsSubstringsSet.has(input)) {
+      isInputIncorrect = false;
+    } else {
+      isInputIncorrect = true;
+    }
     this.setState({isInputIncorrect: isInputIncorrect});
+
+    return { inputComplete: false };
+  },
+  handleInputComplete: function() {
+    var newPosition = this.state.currentSentencePosition + 1;
+    var isSentenceComplete = false;
+
+    if (this.state.activeSentenceCharacterGroups.length < newPosition + 1) {
+      isSentenceComplete = true;
+    } else {
+      this.setActiveCharacterGroup(newPosition, this.state.activeSentenceCharacterGroups);
+    }
+    return { inputComplete: true, sentenceComplete: isSentenceComplete };
+  },
+  handleSentenceComplete: function() {
+    this.setActiveSentence();
   },
   kanaToRomaji: function(kana) {
     // add little tsu + character cases to table
     // add other correct romaji entries (values to be arrays)
     // add katakana equivalent
+    // punctuation
     var kanaTable = {
-      あ: 'a',
-      い: 'i',
-      う: 'u',
-      え: 'e',
-      お: 'o',
-      ゔぁ: 'va',
-      ゔぃ: 'vi',
-      ゔ: 'vu',
-      ゔぇ: 've',
-      ゔぉ: 'vo',
-      か: 'ka',
-      き: 'ki',
-      きゃ: 'kya',
-      きぃ: 'kyi',
-      きゅ: 'kyu',
-      く: 'ku',
-      け: 'ke',
-      こ: 'ko',
-      が: 'ga',
-      ぎ: 'gi',
-      ぐ: 'gu',
-      げ: 'ge',
-      ご: 'go',
-      ぎゃ: 'gya',
-      ぎぃ: 'gyi',
-      ぎゅ: 'gyu',
-      ぎぇ: 'gye',
-      ぎょ: 'gyo',
-      さ: 'sa',
-      す: 'su',
-      せ: 'se',
-      そ: 'so',
-      ざ: 'za',
-      ず: 'zu',
-      ぜ: 'ze',
-      ぞ: 'zo',
-      し: 'shi',
-      しゃ: 'sha',
-      しゅ: 'shu',
-      しょ: 'sho',
-      じ: 'ji',
-      じゃ: 'ja',
-      じゅ: 'ju',
-      じょ: 'jo',
-      た: 'ta',
-      ち: 'chi',
-      ちゃ: 'cha',
-      ちゅ: 'chu',
-      ちょ: 'cho',
-      つ: 'tsu',
-      て: 'te',
-      と: 'to',
-      だ: 'da',
-      ぢ: 'di',
-      づ: 'du',
-      で: 'de',
-      ど: 'do',
-      な: 'na',
-      に: 'ni',
-      にゃ: 'nya',
-      にゅ: 'nyu',
-      にょ: 'nyo',
-      ぬ: 'nu',
-      ね: 'ne',
-      の: 'no',
-      は: 'ha',
-      ひ: 'hi',
-      ふ: 'fu',
-      へ: 'he',
-      ほ: 'ho',
-      ひゃ: 'hya',
-      ひゅ: 'hyu',
-      ひょ: 'hyo',
-      ふぁ: 'fa',
-      ふぃ: 'fi',
-      ふぇ: 'fe',
-      ふぉ: 'fo',
-      ば: 'ba',
-      び: 'bi',
-      ぶ: 'bu',
-      べ: 'be',
-      ぼ: 'bo',
-      びゃ: 'bya',
-      びゅ: 'byu',
-      びょ: 'byo',
-      ぱ: 'pa',
-      ぴ: 'pi',
-      ぷ: 'pu',
-      ぺ: 'pe',
-      ぽ: 'po',
-      ぴゃ: 'pya',
-      ぴゅ: 'pyu',
-      ぴょ: 'pyo',
-      ま: 'ma',
-      み: 'mi',
-      む: 'mu',
-      め: 'me',
-      も: 'mo',
-      みゃ: 'mya',
-      みゅ: 'myu',
-      みょ: 'myo',
-      や: 'ya',
-      ゆ: 'yu',
-      よ: 'yo',
-      ら: 'ra',
-      り: 'ri',
-      る: 'ru',
-      れ: 're',
-      ろ: 'ro',
-      りゃ: 'rya',
-      りゅ: 'ryu',
-      りょ: 'ryo',
-      わ: 'wa',
-      を: 'wo',
-      ん: 'n',
-      ゐ: 'wi',
-      ゑ: 'we',
-      きぇ: 'kye',
-      きょ: 'kyo',
-      じぃ: 'jyi',
-      じぇ: 'jye',
-      ちぃ: 'cyi',
-      ちぇ: 'che',
-      ひぃ: 'hyi',
-      ひぇ: 'hye',
-      びぃ: 'byi',
-      びぇ: 'bye',
-      ぴぃ: 'pyi',
-      ぴぇ: 'pye',
-      みぇ: 'mye',
-      みぃ: 'myi',
-      りぃ: 'ryi',
-      りぇ: 'rye',
-      にぃ: 'nyi',
-      にぇ: 'nye',
-      しぃ: 'syi',
-      しぇ: 'she',
-      いぇ: 'ye',
-      うぁ: 'wha',
-      うぉ: 'who',
-      うぃ: 'wi',
-      うぇ: 'we',
-      ゔゃ: 'vya',
-      ゔゅ: 'vyu',
-      ゔょ: 'vyo',
-      すぁ: 'swa',
-      すぃ: 'swi',
-      すぅ: 'swu',
-      すぇ: 'swe',
-      すぉ: 'swo',
-      くゃ: 'qya',
-      くゅ: 'qyu',
-      くょ: 'qyo',
-      くぁ: 'qwa',
-      くぃ: 'qwi',
-      くぅ: 'qwu',
-      くぇ: 'qwe',
-      くぉ: 'qwo',
-      ぐぁ: 'gwa',
-      ぐぃ: 'gwi',
-      ぐぅ: 'gwu',
-      ぐぇ: 'gwe',
-      ぐぉ: 'gwo',
-      つぁ: 'tsa',
-      つぃ: 'tsi',
-      つぇ: 'tse',
-      つぉ: 'tso',
-      てゃ: 'tha',
-      てぃ: 'thi',
-      てゅ: 'thu',
-      てぇ: 'the',
-      てょ: 'tho',
-      とぁ: 'twa',
-      とぃ: 'twi',
-      とぅ: 'twu',
-      とぇ: 'twe',
-      とぉ: 'two',
-      ぢゃ: 'dya',
-      ぢぃ: 'dyi',
-      ぢゅ: 'dyu',
-      ぢぇ: 'dye',
-      ぢょ: 'dyo',
-      でゃ: 'dha',
-      でぃ: 'dhi',
-      でゅ: 'dhu',
-      でぇ: 'dhe',
-      でょ: 'dho',
-      どぁ: 'dwa',
-      どぃ: 'dwi',
-      どぅ: 'dwu',
-      どぇ: 'dwe',
-      どぉ: 'dwo',
-      ふぅ: 'fwu',
-      ふゃ: 'fya',
-      ふゅ: 'fyu',
-      ふょ: 'fyo'
+      あ: ['a'],
+      い: ['i'],
+      う: ['u'],
+      え: ['e'],
+      お: ['o'],
+      ゔぁ: ['va'],
+      ゔぃ: ['vi'],
+      ゔ: ['vu'],
+      ゔぇ: ['ve'],
+      ゔぉ: ['vo'],
+      か: ['ka'],
+      き: ['ki'],
+      きゃ: ['kya'],
+      きぃ: ['kyi'],
+      きゅ: ['kyu'],
+      く: ['ku'],
+      け: ['ke'],
+      こ: ['ko'],
+      が: ['ga'],
+      ぎ: ['gi'],
+      ぐ: ['gu'],
+      げ: ['ge'],
+      ご: ['go'],
+      ぎゃ: ['gya'],
+      ぎぃ: ['gyi'],
+      ぎゅ: ['gyu'],
+      ぎぇ: ['gye'],
+      ぎょ: ['gyo'],
+      さ: ['sa'],
+      す: ['su'],
+      せ: ['se'],
+      そ: ['so'],
+      ざ: ['za'],
+      ず: ['zu'],
+      ぜ: ['ze'],
+      ぞ: ['zo'],
+      し: ['shi', 'si'],
+      しゃ: ['sha'],
+      しゅ: ['shu'],
+      しょ: ['sho'],
+      じ: ['ji'],
+      じゃ: ['ja', 'jya'],
+      じゅ: ['ju', 'jyu'],
+      じょ: ['jo', 'jyo'],
+      た: ['ta'],
+      ち: ['chi'],
+      ちゃ: ['cha'],
+      ちゅ: ['chu'],
+      ちょ: ['cho'],
+      つ: ['tsu'],
+      て: ['te'],
+      と: ['to'],
+      だ: ['da'],
+      ぢ: ['di'],
+      づ: ['du'],
+      で: ['de'],
+      ど: ['do'],
+      な: ['na'],
+      に: ['ni'],
+      にゃ: ['nya'],
+      にゅ: ['nyu'],
+      にょ: ['nyo'],
+      ぬ: ['nu'],
+      ね: ['ne'],
+      の: ['no'],
+      は: ['ha'],
+      ひ: ['hi'],
+      ふ: ['fu'],
+      へ: ['he'],
+      ほ: ['ho'],
+      ひゃ: ['hya'],
+      ひゅ: ['hyu'],
+      ひょ: ['hyo'],
+      ふぁ: ['fa'],
+      ふぃ: ['fi'],
+      ふぇ: ['fe'],
+      ふぉ: ['fo'],
+      ば: ['ba'],
+      び: ['bi'],
+      ぶ: ['bu'],
+      べ: ['be'],
+      ぼ: ['bo'],
+      びゃ: ['bya'],
+      びゅ: ['byu'],
+      びょ: ['byo'],
+      ぱ: ['pa'],
+      ぴ: ['pi'],
+      ぷ: ['pu'],
+      ぺ: ['pe'],
+      ぽ: ['po'],
+      ぴゃ: ['pya'],
+      ぴゅ: ['pyu'],
+      ぴょ: ['pyo'],
+      ま: ['ma'],
+      み: ['mi'],
+      む: ['mu'],
+      め: ['me'],
+      も: ['mo'],
+      みゃ: ['mya'],
+      みゅ: ['myu'],
+      みょ: ['myo'],
+      や: ['ya'],
+      ゆ: ['yu'],
+      よ: ['yo'],
+      ら: ['ra'],
+      り: ['ri'],
+      る: ['ru'],
+      れ: ['re'],
+      ろ: ['ro'],
+      りゃ: ['rya'],
+      りゅ: ['ryu'],
+      りょ: ['ryo'],
+      わ: ['wa'],
+      を: ['wo'],
+      ん: ['nn', 'n '],
+      ゐ: ['wi'],
+      ゑ: ['we'],
+      きぇ: ['kye'],
+      きょ: ['kyo'],
+      じぃ: ['jyi'],
+      じぇ: ['jye'],
+      ちぃ: ['cyi'],
+      ちぇ: ['che'],
+      ひぃ: ['hyi'],
+      ひぇ: ['hye'],
+      びぃ: ['byi'],
+      びぇ: ['bye'],
+      ぴぃ: ['pyi'],
+      ぴぇ: ['pye'],
+      みぇ: ['mye'],
+      みぃ: ['myi'],
+      りぃ: ['ryi'],
+      りぇ: ['rye'],
+      にぃ: ['nyi'],
+      にぇ: ['nye'],
+      しぃ: ['syi'],
+      しぇ: ['she'],
+      いぇ: ['ye'],
+      うぁ: ['wha'],
+      うぉ: ['who'],
+      うぃ: ['wi'],
+      うぇ: ['we'],
+      ゔゃ: ['vya'],
+      ゔゅ: ['vyu'],
+      ゔょ: ['vyo'],
+      すぁ: ['swa'],
+      すぃ: ['swi'],
+      すぅ: ['swu'],
+      すぇ: ['swe'],
+      すぉ: ['swo'],
+      くゃ: ['qya'],
+      くゅ: ['qyu'],
+      くょ: ['qyo'],
+      くぁ: ['qwa'],
+      くぃ: ['qwi'],
+      くぅ: ['qwu'],
+      くぇ: ['qwe'],
+      くぉ: ['qwo'],
+      ぐぁ: ['gwa'],
+      ぐぃ: ['gwi'],
+      ぐぅ: ['gwu'],
+      ぐぇ: ['gwe'],
+      ぐぉ: ['gwo'],
+      つぁ: ['tsa'],
+      つぃ: ['tsi'],
+      つぇ: ['tse'],
+      つぉ: ['tso'],
+      てゃ: ['tha'],
+      てぃ: ['thi'],
+      てゅ: ['thu'],
+      てぇ: ['the'],
+      てょ: ['tho'],
+      とぁ: ['twa'],
+      とぃ: ['twi'],
+      とぅ: ['twu'],
+      とぇ: ['twe'],
+      とぉ: ['two'],
+      ぢゃ: ['dya'],
+      ぢぃ: ['dyi'],
+      ぢゅ: ['dyu'],
+      ぢぇ: ['dye'],
+      ぢょ: ['dyo'],
+      でゃ: ['dha'],
+      でぃ: ['dhi'],
+      でゅ: ['dhu'],
+      でぇ: ['dhe'],
+      でょ: ['dho'],
+      どぁ: ['dwa'],
+      どぃ: ['dwi'],
+      どぅ: ['dwu'],
+      どぇ: ['dwe'],
+      どぉ: ['dwo'],
+      ふぅ: ['fwu'],
+      ふゃ: ['fya'],
+      ふゅ: ['fyu'],
+      ふょ: ['fyo'],
+      '、': [','],
+      '。': ['.'],
+      '！': ['!']
     }
     return kanaTable[kana]
   },
@@ -249,31 +294,31 @@ var AppBox = React.createClass({
     var sentences = this.state.sentences;
     return sentences[Math.floor(Math.random()*sentences.length)];
   },
-  setActiveCharacterGroup: function(position) {
-    var activeCharacterGroup = this.state.activeSentenceCharacterGroups[position];
-    var activeCharacterGroupRomaji = this.kanaToRomaji(activeCharacterGroup);
-    var activeCharacterGroupSet = {
+  setActiveCharacterGroup: function(position, sentenceCharacterGroups) {
+    var activeCharacterGroup = sentenceCharacterGroups[position];
+    var activeCharacterGroupRomajis = this.kanaToRomaji(activeCharacterGroup);
+    var activeCharacterGroupInfo = {
       kana: activeCharacterGroup,
-      romaji: activeCharacterGroupRomaji
+      romajis: activeCharacterGroupRomajis
     }
 
     this.setState({
-      activeCharacterGroupSet: activeCharacterGroupSet,
-      setCurrentSentencePosition: position
+      activeCharacterGroup: activeCharacterGroupInfo,
+      currentSentencePosition: position,
+      isInputIncorrect: false
     });
   },
   setActiveSentence: function() {
     var _this = this;
     var randomSentence = this.selectRandomSentence();
     var randomSentenceKana = randomSentence.kana;
-    // var randomSentenceCharacterGroups = randomSentence.kana.split('');
     var randomSentenceCharacterGroups = [];
 
     while (randomSentenceKana.length > 0) {
       var potentialGroup = randomSentenceKana.substring(0,2);
-      var potentialGroupRomaji = _this.kanaToRomaji(potentialGroup);
+      var potentialGroupRomajiSet = _this.kanaToRomaji(potentialGroup);
 
-      if (potentialGroupRomaji) {
+      if (potentialGroupRomajiSet) {
         randomSentenceCharacterGroups.push(potentialGroup);
         randomSentenceKana = randomSentenceKana.slice(2);
       } else {
@@ -281,19 +326,19 @@ var AppBox = React.createClass({
         randomSentenceKana = randomSentenceKana.slice(1);
       }
     }
-    console.log(randomSentenceKana);
+
     this.setState({
       activeSentence: randomSentence,
       activeSentenceCharacterGroups: randomSentenceCharacterGroups
     });
 
-    this.setActiveCharacterGroup(0);
+    this.setActiveCharacterGroup(0, randomSentenceCharacterGroups);
   },
   render: function() {
     return (
       <div className="app-box">
         <VisualFeedback characterGroups={this.state.activeSentenceCharacterGroups} inputIncorrect={this.state.isInputIncorrect} currentSentencePosition={this.state.currentSentencePosition} />
-        <UserInput onInputCheck={this.handleInputCheck} inputIncorrect={this.state.isInputIncorrect} />
+        <UserInput onInputCheck={this.handleInputCheck} onSentenceComplete={this.handleSentenceComplete} inputIncorrect={this.state.isInputIncorrect} />
         <SentenceInformation sentence={this.state.activeSentence} />
       </div>
     );
@@ -340,7 +385,15 @@ var UserInput = React.createClass({
   },
   handleUserInputChange: function(e) {
     this.setState({userInput: e.target.value});
-    this.props.onInputCheck(e.target.value);
+    var inputCheck = this.props.onInputCheck(e.target.value);
+
+    if (inputCheck.inputComplete) {
+      this.setState({userInput: ''});
+    }
+
+    if (inputCheck.sentenceComplete) {
+      this.props.onSentenceComplete();
+    }
   },
   handleSubmit: function(e) {
     e.preventDefault();
